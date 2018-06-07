dfccdc433d9af0b9e056fd63639ba32e76886187
Martin Villalba <14tinchov@gmail.com>
2018-06-07 10:33:03 -0300

    Change components to class

--- src/components/product_view/ProductView.jsx
+++ src/components/product_view/ProductView.jsx
@@ -3,19 +3,21 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import ProductList from '../product_list/ProductList';
 import ShoppingCart from '../shopping_cart/ShoppingCart';
 
-const ProductView = () => {
-  return(
-    <Grid>
-      <Row className="show-grid">
-        <Col md={9}>
-          <ProductList />
-        </Col>
-        <Col md={3}>
-          <ShoppingCart/>
-        </Col>
-      </Row>
-    </Grid>
-  )
+class ProductView extends React.Component {
+  render (){
+    return(
+      <Grid>
+        <Row className="show-grid">
+          <Col md={9}>
+            <ProductList />
+          </Col>
+          <Col md={3}>
+            <ShoppingCart/>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
 }
 
 export default ProductView
